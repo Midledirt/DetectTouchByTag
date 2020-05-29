4c48da61a9dc2bfd1bb2bd335350fedbc7ad585a
Jonathan <jonpres@icloud.com>
2020-05-29 16:15:56 +0200

    The script is now set up to detect thee touch position and store it.

--- Assets/Scripts/scrDetectTouchByTag.cs
+++ Assets/Scripts/scrDetectTouchByTag.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class scrDetectTouchByTag : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Vector3 touchPositionWorld;
 
-    // Update is called once per frame
+    //The game object we touch
+    public GameObject touchedObject;
+
+    //Edit this touchphase to alter which touchphase is used for the check
+    TouchPhase editableTouchphase = TouchPhase.Ended;
+
     void Update()
     {
-        
+        //Check for finger touches, take the first touch (GetTouch(0))and check its status.
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == editableTouchphase)
+        {
+            //We get the position of the first touch in "world position"...
+            touchPositionWorld = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+
+            //And store it as a vector2!
+            Vector2 touchPositionWOrld2D = new Vector2(touchPositionWorld.x, touchPositionWorld.y);
+
+        }
+
     }
-}
+
+} 
+
